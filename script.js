cd20eea2721945492323b4f5fd53057e7b2f2739
chenpetro <chenpetro@gmail.com>
2025-01-28 20:15:17 +0100

    new commit

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function getTime(endtime) {
     // let newTime = new Date(2024, 1, 1)
     // console.log(newTime);
      let time = Date.parse(deadline) - Date.parse(new Date())
-    console.log(Date.parse(deadline));
-    console.log(Date.parse(new Date()));
-    console.log(time);
+    // console.log(Date.parse(deadline));
+    // console.log(Date.parse(new Date()));
+    // console.log(time);
 // let seconds = Math.floor((time / 1000) % 60);
 // let minutes = Math.floor((time / (1000 * 60)) % 60);
 // let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
@@ -16,11 +16,6 @@ function getTime(endtime) {
     let minutes = Math.floor((time / 1000/60) % 60)
     let hours = Math.floor(time / (1000*60*60)) % 24
     let days = Math.floor((time / (1000*60*60*24)))
-    
-    console.log(seconds);
-    console.log(minutes);
-    console.log(hours);
-    console.log(days);
 
     return {
         'total': time,
@@ -35,4 +30,23 @@ function getTime(endtime) {
     
 }
 
-getTime()
+function setTime(id, endtime){
+let timer1 = document.getElementById(id)
+let seconds = timer1.querySelector('.seconds')
+let minutes = timer1.querySelector('.minutes')
+let days = timer1.querySelector('.days')
+timeInterval = setInterval(updateTime, 1000)
+
+function updateTime(){
+    let time = getTime(endtime)    
+    seconds.textContent = `${time.seconds} seconds`;
+    minutes.textContent = `${time.minutes} minutes`;
+    hours.textContent = `${time.hours} hours`;
+    days.textContent = `${time.days} days`;
+}
+
+updateTime()
+
+}
+
+setTime('timer1', deadline)
